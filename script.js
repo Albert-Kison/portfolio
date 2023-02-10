fa093b8ab970f882376a5fb7dc919fd573461d97
Albert Kison <albert.kison@student.griffith.ie>
2023-02-10 14:14:27 +0100

    Add links section and footer

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const heading = document.querySelector("h1");
+const footer = document.querySelector("footer p");
 
-let a = 0;
-console.log(a);
-
-heading.addEventListener("click", function() {
-    a = 4;
-    console.log(a);
-});
-
-heading.addEventListener("click", function() {
-    a = 10;
-    console.log(a);
-});
-
-console.log(a);
-
-function A() {
-    this.x = 9;
-    console.log(this);
-}
-
-const b = new A();
+footer.innerHTML = "&copy; " + new Date().getFullYear();
